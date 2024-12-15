bd97aa36897b7a49d78acad3ac72612dfe340dd5
hariomsharmacse <hariomsharmacse@gmail.com>
2024-12-15 11:27:51 +0530

    title

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -2,7 +2,7 @@ export default function AboutMe(){
     return(
         <section className="mb-20 md:mb-40" >
             <h2 className="text-3xl mb-4 text-center" >About Me</h2>
-            <p className="text-center sm:text-lg lg:text-xl max-w-3xl mx-auto text-zinc-600 dark:text-zinc-400" >I am a passionate and dedicated front-end developer with a strong foundation in web development technologies and a keen eye for design. Having recently achieved frontend skills, I am eager to apply my skills and knowledge in a professional setting to create engaging, user-friendly web applications.</p>
+            <p className="text-center sm:text-lg lg:text-xl max-w-3xl mx-auto text-zinc-600 dark:text-zinc-400" >I am a passionate and dedicated Full-Stack Web Developer with a strong foundation in web development technologies and a keen eye for design. Having recently achieved frontend, backend skills, I am eager to apply my skills and knowledge in a professional setting to create engaging, user-friendly web applications.</p>
         </section>
     )
 }
