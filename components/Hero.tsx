52aeda0a401d2867d1bef8430289756210625105
hariomsharmacse <hariomsharmacse@gmail.com>
2025-02-12 13:42:18 +0530

    name change on hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero(){
     return(
         <section className='text-center mb-48 mt-32' >
           <p className='sm:text-lg' >Hi there,</p>
-          <h1 className='text-6xl sm:text-8xl font-bold text-center dark:text-white my-2 mb-4' >I am <span className="text-blue-600" > Hariom </span> </h1>
+          <h1 className='text-6xl sm:text-8xl font-bold text-center dark:text-white my-2 mb-4' >I am <span className="text-blue-600" > Vishal </span> </h1>
           <h2 className='text-xl sm:text-3xl mb-4' >{"< Full-Stack Web Developer />"}</h2>
           {/* <div className="flex items-center justify-center gap-4" >
                 <FaGithub size={28} />
